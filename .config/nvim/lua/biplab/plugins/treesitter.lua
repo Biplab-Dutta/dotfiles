3cd4c3ba887b99aa43ddb706db97ebc9bdc0f4a0
Biplab Dutta <biplabdutta860@gmail.com>
2025-02-08 16:01:18 +0545

    add standalone treesitter-textobjects plugin for easy management

--- .config/nvim/lua/biplab/plugins/treesitter.lua
+++ .config/nvim/lua/biplab/plugins/treesitter.lua
@@ -7,37 +7,32 @@ return {
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
   config = function()
-    -- import nvim-treesitter plugin
     local treesitter = require 'nvim-treesitter.configs'
 
-    -- configure treesitter
-    treesitter.setup { -- enable syntax highlighting
+    treesitter.setup {
       highlight = {
         enable = true,
       },
-      -- enable indentation
       indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
       autotag = {
         enable = true,
       },
-      -- ensure these language parsers are installed
       ensure_installed = {
         'json',
-        'javascript',
-        'typescript',
-        'tsx',
+        -- 'javascript',
+        -- 'typescript',
+        -- 'tsx',
         'yaml',
-        'html',
-        'css',
-        'markdown',
-        'markdown_inline',
+        -- 'html',
+        -- 'css',
+        -- 'markdown',
+        -- 'markdown_inline',
         'bash',
         'lua',
         'vim',
         'dockerfile',
         'gitignore',
-        'vimdoc',
+        -- 'vimdoc',
         'go',
         'c',
         'cpp',
@@ -49,25 +44,6 @@ return {
           node_incremental = '<C-space>',
           scope_incremental = false,
           node_decremental = '<bs>',
-        },
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
-            ['as'] = { query = '@local.scope', query_group = 'locals', desc = 'Select language scope' },
-          },
-          selection_modes = {
-            ['@parameter.outer'] = 'v', -- charwise
-            ['@function.outer'] = 'V', -- linewise
-            ['@class.outer'] = '<c-v>', -- blockwise
-          },
-          include_surrounding_whitespace = true,
         },
       },
     }
