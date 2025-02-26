3795eb7ea8eeace1c729aef1bcef70431a2a8ee6
Biplab Dutta <biplabdutta860@gmail.com>
2025-02-26 20:05:18 +0545

    added a plugin for search and replace functionality in nvim

--- .config/nvim/lua/biplab/core/keymaps.lua
+++ .config/nvim/lua/biplab/core/keymaps.lua
@@ -75,3 +75,16 @@ vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open float
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 vim.keymap.set('i', 'kj', '<ESC>', { noremap = true })
+
+vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
+  desc = 'Toggle Spectre',
+})
+vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+  desc = 'Search current word',
+})
+vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+  desc = 'Search current word',
+})
+vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+  desc = 'Search on current file',
+})
