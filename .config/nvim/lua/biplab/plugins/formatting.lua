df1f22467bf173b7c132bde2cb71e38455cedcca
Biplab Dutta <biplabdutta860@gmail.com>
2025-03-02 01:59:50 +0545

    use prettier for json file formatting

--- .config/nvim/lua/biplab/plugins/formatting.lua
+++ .config/nvim/lua/biplab/plugins/formatting.lua
@@ -8,6 +8,7 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         go = { 'gofmt' },
+        json = { 'prettier' },
       },
       format_on_save = {
         lsp_fallback = true,
