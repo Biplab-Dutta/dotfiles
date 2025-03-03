3d73ae3251d4990646145b7a56430d4bcbec01d4
Biplab Dutta <biplabdutta860@gmail.com>
2025-03-03 06:43:51 +0545

    add aerial for code outline window

---
+++ .config/nvim/lua/biplab/plugins/aerial.lua
@@ -0,0 +1,17 @@
+return {
+  'stevearc/aerial.nvim',
+  opts = {},
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons',
+  },
+
+  config = function()
+    require('aerial').setup {
+      layout = {
+        default_direction = 'left',
+      },
+    }
+    vim.keymap.set('n', '<leader>a', '<cmd>AerialToggle!<CR>')
+  end,
+}
