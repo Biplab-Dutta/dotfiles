5923bacd03df9b4c1c858f3ee218f40cfb4056be
Biplab Dutta <biplabdutta860@gmail.com>
2025-03-16 00:06:56 +0545

    add reusable mason tools

--- .config/nvim/lua/biplab/plugins/lsp/mason.lua
+++ .config/nvim/lua/biplab/plugins/lsp/mason.lua
@@ -5,14 +5,10 @@ return {
     'WhoIsSethDaniel/mason-tool-installer.nvim',
   },
   config = function()
-    -- import mason
     local mason = require 'mason'
-    -- import mason-lspconfig
     local mason_lspconfig = require 'mason-lspconfig'
-
     local mason_tool_installer = require 'mason-tool-installer'
 
-    -- enable mason and configure icons
     mason.setup {
       ui = {
         icons = {
@@ -24,16 +20,22 @@ return {
     }
 
     mason_lspconfig.setup {
-      -- list of servers for mason to install
       ensure_installed = {
         'lua_ls',
         'clangd',
         'gopls',
+        'yamlls',
       },
     }
 
     mason_tool_installer.setup {
-      ensure_installed = {},
+      ensure_installed = {
+        'stylua',
+        'prettier',
+        'gofumpt',
+        'goimports-reviser',
+        'golines',
+      },
     }
   end,
 }
