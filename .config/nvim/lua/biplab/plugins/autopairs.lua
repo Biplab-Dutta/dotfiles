608a86bbfc28693a0f19d0e63f20d5ca66dd6037
Biplab Dutta <biplabdutta860@gmail.com>
2025-03-19 18:14:51 +0545

    remove unnecessary config and comments from autopairs.lua file

--- .config/nvim/lua/biplab/plugins/autopairs.lua
+++ .config/nvim/lua/biplab/plugins/autopairs.lua
@@ -5,7 +5,6 @@ return {
     'hrsh7th/nvim-cmp',
   },
   config = function()
-    -- import nvim-autopairs
     local autopairs = require 'nvim-autopairs'
 
     -- configure autopairs
@@ -14,7 +13,6 @@ return {
       ts_config = {
         lua = { 'string' }, -- don't add pairs in lua string treesitter nodes
         javascript = { 'template_string' }, -- don't add pairs in javscript template_string treesitter nodes
-        java = false, -- don't check treesitter on java
       },
     }
 
