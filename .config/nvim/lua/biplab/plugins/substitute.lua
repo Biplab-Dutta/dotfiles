bd16ebce74173b478ad49434fb38bd3d8656513c
Biplab Dutta <biplabdutta860@gmail.com>
2025-03-26 05:12:48 +0545

    set config for exchange ability via substitute plugin in neovim

--- .config/nvim/lua/biplab/plugins/substitute.lua
+++ .config/nvim/lua/biplab/plugins/substitute.lua
@@ -3,15 +3,20 @@ return {
   event = { 'BufReadPre', 'BufNewFile' },
   config = function()
     local substitute = require 'substitute'
+    local exchange = require 'substitute.exchange'
 
     substitute.setup()
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+    local keymap = vim.keymap
 
     keymap.set('n', 's', substitute.operator, { desc = 'Substitute with motion' })
     keymap.set('n', 'ss', substitute.line, { desc = 'Substitute line' })
     keymap.set('n', 'S', substitute.eol, { desc = 'Substitute to end of line' })
     keymap.set('x', 's', substitute.visual, { desc = 'Substitute in visual mode' })
+
+    keymap.set('n', 'cx', exchange.operator, { noremap = true })
+    keymap.set('n', 'cxx', exchange.line, { noremap = true })
+    keymap.set('x', 'cX', exchange.visual, { noremap = true })
+    keymap.set('n', 'cxc', exchange.cancel, { noremap = true })
   end,
 }
