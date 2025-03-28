cd875f59aef923c31211e9f620492d9a1a127403
Biplab Dutta <biplabdutta860@gmail.com>
2025-03-29 02:25:12 +0545

    remove commented code from completion.lua file

--- .config/nvim/lua/biplab/plugins/completion.lua
+++ .config/nvim/lua/biplab/plugins/completion.lua
@@ -97,29 +97,3 @@ return {
     end,
   },
 }
-
--- return {
---   'saghen/blink.cmp',
---   dependencies = { {
---     'L3MON4D3/LuaSnip',
---     version = 'v2.*',
---   }, 'rafamadriz/friendly-snippets' },
---   version = '1.*',
---   opts_extend = { 'sources.default' },
---   opts = {
---     completion = {
---       menu = {
---         min_width = 1,
---         draw = {
---           columns = { { 'label', 'label_description', gap = 1 }, { 'kind' } },
---         },
---       },
---     },
---     sources = {
---       default = { 'lsp', 'path', 'snippets', 'buffer' },
---     },
---     cmdline = {
---       enabled = false,
---     },
---   },
--- }
