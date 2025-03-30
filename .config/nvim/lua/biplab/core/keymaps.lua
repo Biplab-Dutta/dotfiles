206ceb25e8018108dab3352a39e06bad5aa34fd3
Biplab Dutta <biplabdutta860@gmail.com>
2025-03-30 22:53:05 +0545

    update keymappings

--- .config/nvim/lua/biplab/core/keymaps.lua
+++ .config/nvim/lua/biplab/core/keymaps.lua
@@ -2,8 +2,10 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+local map = vim.keymap.set
+
 -- Disable the spacebar key's default behavior in Normal and Visual modules
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+map({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 --For conciseness
 local opts = { noremap = true, silent = true }
@@ -13,99 +15,104 @@ local function map_options(extra_opts)
 end
 
 -- Save File
-vim.keymap.set({ 'n', 'i' }, '<C-s>', '<cmd> w <CR>', opts)
+map({ 'n', 'i' }, '<C-s>', '<cmd> w <CR>', opts)
 
 -- Save file without auto-formatting
-vim.keymap.set({ 'n', 'i' }, '<leader>sn', '<cmd>noautocmd w <CR>', opts)
+map({ 'n', 'i' }, '<leader>sn', '<cmd>noautocmd w <CR>', opts)
 
-vim.keymap.set('n', '<C-i>', '<C-a>', map_options { desc = 'Increment numbers' })
+map('n', '<C-i>', '<C-a>', map_options { desc = 'Increment numbers' })
 
-vim.keymap.set('n', 'gn', 'm`o<Esc>``', map_options { desc = 'Insert new line below' })
-vim.keymap.set('n', 'gN', 'm`O<Esc>``', map_options { desc = 'Insert new line above' })
+map('n', 'gn', 'm`o<Esc>``', map_options { desc = 'Insert new line below' })
+map('n', 'gN', 'm`O<Esc>``', map_options { desc = 'Insert new line above' })
 
-vim.keymap.set('n', '<CR>', 'o<Esc>', map_options { desc = 'Go to new line below' })
-vim.keymap.set('n', '<S-CR>', '<S-o><Esc>', map_options { desc = 'Go to new line above' })
+map('i', '<C-h>', '<Left>', { desc = 'move left' })
+map('i', '<C-l>', '<Right>', { desc = 'move right' })
+map('i', '<C-j>', '<Down>', { desc = 'move down' })
+map('i', '<C-k>', '<Up>', { desc = 'move up' })
 
-vim.keymap.set({ 'n', 'v', 'o' }, 'B', '0', opts)
-vim.keymap.set({ 'n', 'v', 'o' }, 'E', '$', opts)
+map('n', '<CR>', 'o<Esc>', map_options { desc = 'Go to new line below' })
+map('n', '<S-CR>', '<S-o><Esc>', map_options { desc = 'Go to new line above' })
+
+map({ 'n', 'v', 'o' }, 'B', '0', opts)
+map({ 'n', 'v', 'o' }, 'E', '$', opts)
 
 -- Quit file
-vim.keymap.set('n', '<C-q>', '<cmd> q <CR>', opts)
+map('n', '<C-q>', '<cmd> q <CR>', opts)
 
 -- Remove hlsearch highlight on Esc
-vim.keymap.set('n', '<Esc>', '<cmd> nohlsearch <CR>', opts)
+map('n', '<Esc>', '<cmd> nohlsearch <CR>', opts)
 
 -- Delete single character without copying into register
-vim.keymap.set('n', 'x', '"_x', opts)
+map('n', 'x', '"_x', opts)
 
 -- map black hole register
-vim.keymap.set({ 'n', 'v' }, '\\', '"_', opts)
+map({ 'n', 'v' }, '\\', '"_', opts)
 
 -- Vertical scroll and center
-vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
-vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
+map('n', '<C-d>', '<C-d>zz', opts)
+map('n', '<C-u>', '<C-u>zz', opts)
 
 -- Find and center
-vim.keymap.set('n', 'n', 'nzzzv', opts)
-vim.keymap.set('n', 'N', 'Nzzzv', opts)
+map('n', 'n', 'nzzzv', opts)
+map('n', 'N', 'Nzzzv', opts)
 
 -- Resize panes with arrows
-vim.keymap.set('n', '<Up>', ':resize -2<CR>', opts)
-vim.keymap.set('n', '<Down>', ':resize +2<CR>', opts)
-vim.keymap.set('n', '<Left>', ':vertical resize -2<CR>', opts)
-vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts)
+map('n', '<Up>', ':resize -2<CR>', opts)
+map('n', '<Down>', ':resize +2<CR>', opts)
+map('n', '<Left>', ':vertical resize -2<CR>', opts)
+map('n', '<Right>', ':vertical resize +2<CR>', opts)
 
 -- Buffers
-vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
-vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
-vim.keymap.set('n', '<leader>x', ':bdelete!<CR>', opts) -- close buffer
-vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
+map('n', '<Tab>', ':bnext<CR>', opts)
+map('n', '<S-Tab>', ':bprevious<CR>', opts)
+map('n', '<leader>x', ':bdelete!<CR>', opts) -- close buffer
+map('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
 
 -- Window management
-vim.keymap.set('n', '<leader>v', '<C-w>v', opts) -- split window vertically
-vim.keymap.set('n', '<leader>h', '<C-w>s', opts) -- split window horizontally
-vim.keymap.set('n', '<leader>se', '<C-w>=', opts) -- make split windows equal width & height
-vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- close current split window
+map('n', '<leader>v', '<C-w>v', opts) -- split window vertically
+map('n', '<leader>h', '<C-w>s', opts) -- split window horizontally
+map('n', '<leader>se', '<C-w>=', opts) -- make split windows equal width & height
+map('n', '<leader>xs', ':close<CR>', opts) -- close current split window
 
 -- Navigate between splits
-vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
-vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', opts)
-vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
-vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)
+map('n', '<C-k>', ':wincmd k<CR>', opts)
+map('n', '<C-j>', ':wincmd j<CR>', opts)
+map('n', '<C-h>', ':wincmd h<CR>', opts)
+map('n', '<C-l>', ':wincmd l<CR>', opts)
 
 -- Tabs
-vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- open new tab
-vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
-vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts) --  go to next tab
-vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts) --  go to previous tab
+map('n', '<leader>to', ':tabnew<CR>', opts) -- open new tab
+map('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
+map('n', '<leader>tn', ':tabn<CR>', opts) --  go to next tab
+map('n', '<leader>tp', ':tabp<CR>', opts) --  go to previous tab
 
 -- Toggle line wrapping
-vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
+map('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
 
 -- Stay in indent mode
-vim.keymap.set('v', '<', '<gv', opts)
-vim.keymap.set('v', '>', '>gv', opts)
+map('v', '<', '<gv', opts)
+map('v', '>', '>gv', opts)
 
 -- Keep last yanked when pasting
-vim.keymap.set('v', 'p', '"_dP', opts)
+map('v', 'p', '"_dP', opts)
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+map('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+map('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+map('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
-vim.keymap.set('i', 'kj', '<ESC>', { noremap = true })
+map('i', 'kj', '<ESC>', { noremap = true })
 
-vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
+map('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
   desc = 'Toggle Spectre',
 })
-vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+map('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
   desc = 'Search current word',
 })
-vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+map('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
   desc = 'Search current word',
 })
-vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+map('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
   desc = 'Search on current file',
 })
