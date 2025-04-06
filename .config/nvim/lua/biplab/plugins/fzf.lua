12d94393c281b7296107f48bab05d2737a28cbd0
Biplab Dutta <biplabdutta860@gmail.com>
2025-04-06 20:31:49 +0545

    update fzf config that involves some keymappings and layout config

--- .config/nvim/lua/biplab/plugins/fzf.lua
+++ .config/nvim/lua/biplab/plugins/fzf.lua
@@ -7,13 +7,16 @@ return {
     local fzf = require 'fzf-lua'
 
     fzf.setup {
-      { 'telescope' },
-      keymap = {
-        builtin = {
-          ['<C-u>'] = 'preview-page-up',
-          ['<C-d>'] = 'preview-page-down',
+      {
+        'fzf-native',
+        winopts = {
+          preview = { default = 'bat' },
         },
+      },
+      keymap = {
         fzf = {
+          ['ctrl-u'] = 'preview-page-up',
+          ['ctrl-d'] = 'preview-page-down',
           ['ctrl-k'] = 'up',
           ['ctrl-j'] = 'down',
           ['ctrl-q'] = 'abort',
@@ -21,25 +24,25 @@ return {
       },
       files = {
         fd_opts = '--type f --hidden --exclude node_modules --exclude .git --exclude .venv',
-        previewer = 'builtin',
+        previewer = 'bat',
       },
       buffers = {
         sort_lastused = true,
-        previewer = 'builtin',
+        previewer = 'bat',
       },
       grep = {
         cmd = 'rg --line-number --column --no-heading --color=always --smart-case',
         rg_opts = '--hidden --glob "!node_modules/*" --glob "!.git/*" --glob "!.venv/*"',
-        previewer = 'builtin',
+        previewer = 'bat',
       },
       live_grep = {
         cmd = 'rg --line-number --column --no-heading --color=always --smart-case',
         rg_opts = '--hidden --glob "!node_modules/*" --glob "!.git/*" --glob "!.venv/*"',
-        previewer = 'builtin',
+        previewer = 'bat',
       },
       git = {
         files = {
-          previewer = true,
+          previewer = 'bat',
         },
       },
       fzf_opts = {
@@ -54,9 +57,8 @@ return {
 
     local keymap = vim.keymap.set
 
-    keymap('n', '<leader>?', fzf.oldfiles, { desc = '[?] Find recently opened files' })
-    keymap('n', '<leader>sb', fzf.buffers, { desc = '[S]earch existing [B]uffers' })
-    keymap('n', '<leader>sm', fzf.marks, { desc = '[S]earch [M]arks' })
+    keymap('n', '<leader>fb', fzf.buffers, { desc = '[S]earch existing [B]uffers' })
+    keymap('n', '<leader>fm', fzf.marks, { desc = '[S]earch [M]arks' })
     keymap('n', '<leader>gf', fzf.git_files, { desc = 'Search [G]it [F]iles' })
     keymap('n', '<leader>gc', fzf.git_commits, { desc = 'Search [G]it [C]ommits' })
     keymap('n', '<leader>gcf', fzf.git_bcommits, { desc = 'Search [G]it [C]ommits for current [F]ile' })
@@ -66,14 +68,14 @@ return {
     keymap('n', '<leader>sh', fzf.help_tags, { desc = '[S]earch [H]elp' })
     keymap('n', '<leader>scw', fzf.grep_cword, { desc = '[S]earch current [W]ord' })
     keymap('n', '<leader>lg', fzf.live_grep, { desc = '[S]earch by [G]rep' })
-    keymap('n', '<leader>sd', fzf.diagnostics_document, { desc = '[S]earch [D]iagnostics' })
-    keymap('n', '<leader>sr', fzf.resume, { desc = '[S]earch [R]esume' })
-    keymap('n', '<leader>s.', fzf.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+    keymap('n', '<leader>fd', fzf.diagnostics_document, { desc = '[S]earch [D]iagnostics' })
+    keymap('n', '<leader>fr', fzf.resume, { desc = '[S]earch [R]esume' })
+    keymap('n', '<leader>fo', fzf.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     keymap('n', '<leader>qf', fzf.quickfix, { desc = 'Show quick fix list' })
     keymap('n', '<leader>ft', function()
-      fzf.grep { cmd = 'rg --column', search = 'TODO|FIXME|NOTE', prompt = 'Todos> ' }
+      fzf.grep { cmd = 'rg --column --line-number', search = 'TODO', prompt = 'Todos> ' }
     end, { desc = 'Find todos' })
-    keymap('n', '<leader>sds', function()
+    keymap('n', '<leader>a', function()
       fzf.lsp_document_symbols {
         symbol_types = { 'Class', 'Function', 'Method', 'Constructor', 'Interface', 'Module', 'Property' },
       }
